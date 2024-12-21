5057c61d32ef9f3465b3474d62823c1e9efcea6d
Felipe Rossi <feliperossi2002@gmail.com>
2024-12-21 13:48:17 +0000

    [day19] Yep. It's wrong. I need epsilons

--- src/main/ml/day19.ml
+++ src/main/ml/day19.ml
@@ -15,57 +15,70 @@ let patterns =
 ;;
 
 (* Read empty input line *)
-In_channel.input_line In_channel.stdin ;;
+In_channel.input_line In_channel.stdin
 
-let designs =
-  In_channel.input_lines In_channel.stdin
-;;
+let designs = In_channel.input_lines In_channel.stdin
 
 (*(*(*(*(*(*(*(*(*( PART 1 )*)*)*)*)*)*)*)*)*)
 let rec part1 patterns designs =
-  let pat_regexp = List.fold patterns ~init:{|^\(|} ~f:(fun acc x -> acc ^ {|\||} ^ x) ^ {|\)*$|} |> Str.regexp in
-  let possible design =
-    if Str.string_match pat_regexp design 0 then 1 else 0
+  let pat_regexp =
+    List.fold patterns ~init:{|^\(|} ~f:(fun acc x -> acc ^ {|\||} ^ x) ^ {|\)*$|}
+    |> Str.regexp
   in
+  let possible design = if Str.string_match pat_regexp design 0 then 1 else 0 in
   match designs with
   | [] -> 0
-  | x::xs -> possible x + part1 patterns xs
+  | x :: xs -> possible x + part1 patterns xs
 ;;
 
 (*(*(*(*(*(*(*(*(*( PART 2 )*)*)*)*)*)*)*)*)*)
 (* Too slow:
-let part2 patterns designs =
-  let open Utils.String in
-  let patterns = List.map ~f:explode patterns in
-  let designs = List.map ~f:explode designs in
-  let rec remove_start start from_str =
-    match start with
-    | [] -> Some from_str
-    | x::xs ->
-        match from_str with
-        | [] -> None
-        | y::ys -> if Char.equal x y then remove_start xs ys else None
-  in
-  let rec possibilities (design: char list) =
-    List.sum (module Int) patterns ~f:(fun pat ->
-      match remove_start pat design with
-      | None -> 0
-      | Some [] -> 1
-      | Some substr -> possibilities substr
-    )
-  in
-  let rec totals designs =
-    match designs with
-    | [] -> 0
-    | x::xs -> possibilities x + totals xs
-  in
-  totals designs
-;;
+   let part2 patterns designs =
+   let open Utils.String in
+   let patterns = List.map ~f:explode patterns in
+   let designs = List.map ~f:explode designs in
+   let rec remove_start start from_str =
+   match start with
+   | [] -> Some from_str
+   | x::xs ->
+   match from_str with
+   | [] -> None
+   | y::ys -> if Char.equal x y then remove_start xs ys else None
+   in
+   let rec possibilities (design: char list) =
+   List.sum (module Int) patterns ~f:(fun pat ->
+   match remove_start pat design with
+   | None -> 0
+   | Some [] -> 1
+   | Some substr -> possibilities substr
+   )
+   in
+   let rec totals designs =
+   match designs with
+   | [] -> 0
+   | x::xs -> possibilities x + totals xs
+   in
+   totals designs
+   ;;
 *)
-let part2 patterns designs = raise (Invalid_argument "Part 2 not solved yet.")
-;;
-
-print_endline Utils.Word_list_to_automata.(words_to_automata (List.map patterns ~f:Utils.Str_utils.explode) |> string_of_automata)
+(* TODO wrong *)
+let part2 patterns designs =
+  let open Utils.Word_list_to_automata in
+  let patterns, designs =
+    Utils.Str_utils.(List.map patterns ~f:explode, List.map designs ~f:explode)
+  in
+  let automata = words_to_automata patterns in
+  print_endline (string_of_automata automata);
+  let rec possibilities state design =
+    let to_states = Map.find_exn automata.transitions state in
+    match design with
+    | [] -> if Set.mem automata.accept_states state then 1 else 0
+    | sym :: subdesign ->
+      match Map.find to_states sym with
+      | None -> 0
+      | Some next_state -> possibilities next_state subdesign
+  in
+  List.sum (module Int) designs ~f:(possibilities automata.init_state)
 ;;
 
 (*(*(*(*(*(*(*(*(*( SOLVE )*)*)*)*)*)*)*)*)*)
@@ -85,4 +98,3 @@ let () =
   in
   print_endline @@ string_of_int @@ solve patterns designs
 ;;
-
