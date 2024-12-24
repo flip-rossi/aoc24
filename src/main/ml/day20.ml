aab7d6d61f4af155e6a167ae9a10c18716a6047b
Felipe Rossi <feliperossi2002@gmail.com>
2024-12-24 00:42:24 +0000

    [day20] Failed attempt to try the same thing that didn't work before

--- src/main/ml/day20.ml
+++ src/main/ml/day20.ml
@@ -10,18 +10,15 @@ type pos =
   { x : int
   ; y : int
   }
+[@@deriving sexp, compare, hash]
 
 module Position = struct
-  type t = pos
+  module T = struct
+    type t = pos [@@deriving sexp, compare, hash]
+  end
 
-  let compare a b =
-    match a.x - b.x with
-    | 0 -> a.y - b.y
-    | comp -> comp
-  ;;
-
-  let sexp_of_t { x; y } = sexp_of_pair sexp_of_int sexp_of_int (x, y)
-  let to_string { x; y } = "{ x = " ^ string_of_int x ^ "; y = " ^ string_of_int y ^ " }"
+  include T
+  include Comparator.Make (T)
 end
 
 let dirs = [ { x = 1; y = 0 }; { x = -1; y = 0 }; { x = 0; y = 1 }; { x = 0; y = -1 } ]
@@ -96,7 +93,84 @@ let part1 start map =
 ;;
 
 (*(*(*(*(*(*(*(*(*( PART 2 )*)*)*)*)*)*)*)*)*)
-let part2 _ = raise (Invalid_argument "Part 2 not solved yet.")
+type cheat_state =
+  | Honest
+  | Cheating of pos * int
+  | Cheated
+
+let cheat_len = 20
+
+module Pos_pair = struct
+  module T = struct
+    type t = Position.t * Position.t [@@deriving sexp, compare, hash]
+  end
+
+  include T
+  include Comparable.Make (T)
+end
+
+let part2 start map =
+  let open Utils in
+  let n, m = Array.length map, Array.length map.(0) in
+  let rec bfs_count
+    count_fun
+    used_cheats
+    found
+    (q : (pos * int * cheat_state) Func_queue.t)
+    =
+    let self = bfs_count count_fun used_cheats in
+    let explore found q node lvl cheat =
+      let found = Set.add found node in
+      let q =
+        List.filter_map dirs ~f:(fun { x = dx; y = dy } ->
+          let x, y = node.x + dx, node.y + dy in
+          if 0 <= y && y < n && 0 <= x && x < m && not (Set.mem found { x; y })
+          then Some ({ x; y }, lvl + 1, cheat)
+          else None)
+        |> Func_queue.append q
+      in
+      self found q
+    in
+    match Func_queue.dequeue q with
+    | _, None -> failwith "No path found."
+    | q, Some (pos, lvl, cheat) ->
+      (match map.(pos.y).(pos.x), cheat with
+       | End, Cheating (p0, _) ->
+         if not (Hash_set.mem used_cheats (p0, pos))
+         then (
+           print_endline @@ "Found exit at lvl " ^ string_of_int lvl;
+           Hash_set.add used_cheats (p0, pos);
+           count_fun lvl)
+         else 0
+       | End, _ ->
+         print_endline @@ "Found exit at lvl " ^ string_of_int lvl;
+         count_fun lvl
+       | Wall, Honest ->
+         self found q + explore found q pos lvl (Cheating (pos, cheat_len - 1))
+       | Wall, Cheating (_, 0) -> (* died in wall *) self found q
+       | Wall, Cheated -> (* died in or hit wall *) self found q
+       | _, Cheating (p0, remaining) ->
+         (* cheat ended *)
+         (if not (Hash_set.mem used_cheats (p0, pos))
+          then (
+            Hash_set.add used_cheats (p0, pos);
+            explore found q pos lvl Cheated)
+          else 0)
+         +
+         if remaining > 0
+         then explore found q pos lvl (Cheating (pos, remaining - 1))
+         else 0
+       | Free, cheat -> explore found q pos lvl cheat)
+  in
+  let queue0 = Func_queue.singleton (start, 0, Cheated) in
+  let found0 = Set.singleton (module Position) start in
+  let used_cheats = Hash_set.create (module Pos_pair) ?size:None ~growth_allowed:true in
+  let count_lvl lvl = lvl in
+  let cheatless_len = bfs_count count_lvl used_cheats found0 queue0 in
+  let count_100_diff lvl = if lvl <= cheatless_len - 100 then 1 else 0 in
+  let queue0 = Func_queue.singleton (start, 0, Honest) in
+  bfs_count count_100_diff used_cheats found0 queue0
+;;
 
 (*(*(*(*(*(*(*(*(*( SOLVE )*)*)*)*)*)*)*)*)*)
 let () =
